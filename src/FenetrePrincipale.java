20e4cfc897ff824e4495ebc454e5af42629a288e
spycro <lucaszim16+git@gmail.com>
2019-02-27 21:49:11 +0100

    Ajout de classe

---
+++ src/FenetrePrincipale.java
@@ -0,0 +1,18 @@
+import java.awt.GraphicsConfiguration;
+
+
+import javax.swing.JFrame;
+
+public class FenetrePrincipale extends JFrame {
+
+	public FenetrePrincipale(){
+		super("SusperStar");
+		
+		
+	
+	
+	}
+
+
+
+}
