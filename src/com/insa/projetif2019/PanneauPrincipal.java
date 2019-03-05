c9577b074f097ba87a3ce3d54db5b27602ef58c2
Loleau <Loleau@users.noreply.github.com>
2019-03-05 16:53:45 +0100

    Update PanneauPrincipal.java

--- src/com/insa/projetif2019/PanneauPrincipal.java
+++ src/com/insa/projetif2019/PanneauPrincipal.java
@@ -1,24 +1,25 @@
 package com.insa.projetif2019;
 /**
- * classe heritant de JPanel qui gerera probablement le menu principal.
- * implementera surement ActionListener pour les jButton qui seront present. 
+ * Menu principal du jeu. Permet de lancer le jeu ou de le quitter
 */
 
 
 import java.awt.*;
-
-
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class PanneauPrincipal extends JPanel{
 
 	private static final long serialVersionUID = 1L;
-	
-
+	private JButton bStart;
+	private JButton bQuit;
 
 	public PanneauPrincipal() {
 		setBackground(Color.LIGHT_GRAY);
+		//Définition du bouton de lancement
+		bStart = new JButton();
 		
+		//Définition du bouton pour quitter
+		bQuit = new JButton();
 	}
 	
 	
