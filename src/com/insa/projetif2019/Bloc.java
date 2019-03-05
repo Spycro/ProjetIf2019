5dbdf3156bb3d2b9eb4b52ddf6f8e2352c6b9ff1
Loleau <Loleau@users.noreply.github.com>
2019-03-05 18:02:12 +0100

    Avancée sur la classe bloc
    Ajout de la gestion des astres pour la génération de la carte

--- src/com/insa/projetif2019/Bloc.java
+++ src/com/insa/projetif2019/Bloc.java
@@ -1,5 +1,11 @@
 package com.insa.projetif2019;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -8,17 +14,19 @@ import java.awt.*;
  * Classe Abstraite Bloc utilise pour faire les sols et les objets 
  * avec lesquels on pourra interragir comme les pics ou les points 
  * d'interrogations.
- * @author Lucas
+ * @author Lucas, Loïc
  * 
  */
 
 public abstract class Bloc {
 	
 	
-	protected static final int COTES = 64;
+	protected static final int COTES = 64; //taille d'un bloc
 	protected Color couleur;
 	protected int coordX;
 	protected int coordY;
+	protected String typeBloc;
+	Image sprite;
 	Rectangle hitBox;
 	
 	
@@ -27,11 +35,132 @@ public abstract class Bloc {
 		couleur = Color.darkGray;
 	}
 	
-	public Bloc(int x, int y) {
+	public Bloc(JFrame parent, int x, int y, int type, String astre) {
+		/**
+		 * astre : Astre pour lequel on demande un bloc
+		 * type : type de bloc demandé
+		 * Description type :
+		 * 1 - coin supérieur gauche
+		 * 2 - sol supérieur plat
+		 * 3 - coin supérieur droit
+		 * 4 - bloc coté gauche
+		 * 5 - bloc intérieur
+		 * 6 - bloc coté droit
+		 */
 		coordY = y;
 		coordX = x;
 		couleur = Color.darkGray;
 		hitBox = new Rectangle(x,y,COTES, COTES);
+		switch(astre){
+		case "terre":
+			try {
+				sprite = ImageIO.read(new File("bin/terre/blocTerre.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			switch(type){
+			
+			}
+			
+			break;
+			
+		case "mars":
+			try {
+				sprite = ImageIO.read(new File("bin/mars/blocMars.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "venus":
+			try {
+				sprite = ImageIO.read(new File("bin/venus/blocVenus.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "mercure":
+			try {
+				sprite = ImageIO.read(new File("bin/mercure/blocMercure.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "jupiter":
+			try {
+				sprite = ImageIO.read(new File("bin/jupiter/blocJupiter.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "saturne":
+			try {
+				sprite = ImageIO.read(new File("bin/saturne/blocSaturne.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "uranus":
+			try {
+				sprite = ImageIO.read(new File("bin/uranus/blocUranus.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "neptune":
+			try {
+				sprite = ImageIO.read(new File("bin/neptune/blocNeptune.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "soleil":
+			try {
+				sprite = ImageIO.read(new File("bin/soleil/blocSoleil.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "lune":
+			try {
+				sprite = ImageIO.read(new File("bin/lune/blocLune.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		case "comete":
+			try {
+				sprite = ImageIO.read(new File("bin/comete/blocComete.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+			
+		default:
+			String err = "Erreur lors du chargement du bloc. Le type : " + this.typeBloc + " n'existe pas !";
+			JOptionPane.showMessageDialog(parent, err);
+			break;
+		}
 	}
 	
 	public Bloc(int x, int y, Color c) {
