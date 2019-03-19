3dfe4721a4286607544621fe9a38930095149709
Janna Attari <jattari@insa-lyon.fr>
2019-03-19 17:34:53 +0100

    fin fenetre info youpiiiiiiiiiiiiiiiiiiiiiii

--- src/com/insa/projetif2019/FenetreInfo.java
+++ src/com/insa/projetif2019/FenetreInfo.java
@@ -12,13 +12,6 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.Font;
 
-/**
- * 
- * 
- *
- */
-
-
 public class FenetreInfo extends JFrame implements ActionListener, KeyListener{
 	
 	private JTextArea info;
@@ -26,33 +19,34 @@ public class FenetreInfo extends JFrame implements ActionListener, KeyListener{
 	private JButton Calcul;
 	private JTextArea balance; 
 	private Planete planete;
+	private Comete comete;
+	private JTextArea annee; 
+	private JTextArea age; 
+
+	private JButton Calcul1;
+	
 	public FenetreInfo(Planete p,int numero) {
 		// paramétrage de la fenetre
 		this.setTitle("A la decouverte de "+p.getNom());
 		this.setLayout(null);
-		this.setSize(960,640);
+		this.setSize(960,200);
 		this.setLocation(700,200);
 		this.setVisible(true);
-		
-		
-
+	
 		planete=p;
 		
-		JLabel couverture = new JLabel(new ImageIcon("bin/Galaxie.jpg"));
-		couverture.setBounds(0,0,960,640);
+		JLabel couverture = new JLabel(new ImageIcon("bin/"+p.getNom()+".planete.jpg"));
+		couverture.setBounds(0,0,960,200);
 		couverture.setLayout(null);
-		
 
-		
-		
 		// Position bouton
 		Calcul = new JButton("Calcul ton poids");
-		Calcul.setBounds(100,200,150,50);
+		Calcul.setBounds(100,100,150,50);
 		Calcul.addActionListener(this);
 		// Position interact
 		balance= new JTextArea();
-		balance.setBounds(300, 200, 100, 50);
-		// Position zone de texte
+		balance.setBounds(300, 100, 100, 50);
+		
 		info= new JTextArea();
 		info.setBounds(10,10,910,600);
 		info.setLineWrap(true);
@@ -87,9 +81,69 @@ public class FenetreInfo extends JFrame implements ActionListener, KeyListener{
 		couverture.add(info);
 		
 		this.add(couverture);
+		repaint();
 		}
 		
+	public FenetreInfo(Comete c, int numero){
+		this.setTitle("A la decouverte de "+c.getNom());
+		this.setLayout(null);
+		this.setSize(960,200);
+		this.setLocation(700,200);
+		this.setVisible(true);
 	
+		comete=c;
+		JLabel couverture = new JLabel(new ImageIcon("bin/06_b1.jpg"));
+		couverture.setBounds(0,0,960,200);
+		couverture.setLayout(null);
+		
+		Calcul1 = new JButton("Quand me verras tu ?");
+		Calcul1.setBounds(100,100,200,50);
+		Calcul1.addActionListener(this);
+		
+		// Position interact
+		annee= new JTextArea();
+		annee.setBounds(400, 100, 100, 50);
+		age= new JTextArea();
+		age.setBounds(600, 100, 100, 50);
+		
+		info= new JTextArea();
+		info.setBounds(10,10,910,600);
+		info.setLineWrap(true);
+		Font police = new Font(" Arial ",Font.BOLD,13);
+		info.setFont(police);
+		info.setForeground(Color.white);
+		this.add(info);
+		switch (numero) {
+		case 1: 
+			info.setText(c.info1());
+			
+		break;
+		case 2:
+			info.setText(c.info2());
+			
+		break;
+		case 3:
+			info.setText(c.info3()); 
+		break;
+		case 4:
+			info.setText(c.info4());
+			couverture.add(Calcul1);
+			couverture.add(annee);
+			couverture.add(age);
+		break;
+		default:
+		break;
+		}
+		info.setEditable(false);
+		info.setOpaque(false);
+		
+		
+		couverture.add(info);
+		
+		this.add(couverture);
+		repaint();
+		
+	}
 	
 	
 
@@ -114,11 +168,15 @@ public void keyTyped(KeyEvent arg0) {
 @Override
 public void actionPerformed(ActionEvent e) {
 	if (e.getSource()==Calcul) {
-		info.setText("Tu es bien curieux, on redoute tous cette question... Combien indique ta balance? Entre un nombre entier");
 		int choix=Integer.parseInt(balance.getText());
-		double poid = planete.calculPoids(choix);
-		info.setText("Sur"+planete.getNom()+"tu peses"+poid);
+		int poid = planete.calculPoids(choix);
+		info.setText("Sur "+planete.getNom()+" tu peses "+poid+" kg");
 	}	
+	if (e.getSource()==Calcul1){
+		int an=Integer.parseInt(annee.getText());
+		int ag =Integer.parseInt(age.getText());
+		info.setText(comete.calculProchaineAp(an,ag));
+	}
 }
 	
 	
--- src/com/insa/projetif2019/test.java
+++ src/com/insa/projetif2019/test.java
@@ -39,7 +39,7 @@ public class test {
 		Planete Uranus=new Planete(25362.0,30687.15,-220,-197,8.68*Math.pow(10,25),"Uranus","de glace");
 		Planete Neptune=new Planete(24622.0,60224.9,-218,-201,1.0247*Math.pow(10,26),"Neptune","de glace");
 		Comete Halley=new Comete("Halley",1994,2,16,76.09,"Edmond Halley");
-		Comete Tempel=new Comete ("Tempel  ",1867,4,3,5.56, "Ernst Wilhelm Tempel");
+		Comete Tempel=new Comete ("Tempel",1867,4,3,5.56, "Ernst Wilhelm Tempel");
 		Comete Tuttle= new Comete (" M�chain-Tuttle",1790,1,9,13.58 ,"Pierre M�chain");
 		//
 	}
