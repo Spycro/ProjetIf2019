2ee1e3ad0a06cf8011089aa187eed9b5da64bb66
Blondicopilote <47972776+Blondicopilote@users.noreply.github.com>
2019-03-26 17:55:31 +0100

    photo

--- src/com/insa/projetif2019/testjaja.java
+++ src/com/insa/projetif2019/testjaja.java
@@ -1,10 +1,10 @@
 package com.insa.projetif2019;
 
 public class testjaja {
-/*public static void main(String[] args) {
-	Planete Terre= new Planete (6371.0,365.25,-88,58,5.97*Math.pow(10,24),"Terre","Tellurique");
-		FenetreInfo i=new FenetreInfo(Terre,1);
+public static void main(String[] args) {
+	Planete Saturne= new Planete (58232.0,10757.0,-189,-139,5.68*Math.pow(10,26),"Saturne","gazeuse");
+	FenetreInfo i=new FenetreInfo(Saturne,1);
 		
 	
-}*/
+}
 }
